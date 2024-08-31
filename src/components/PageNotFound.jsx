448c613137325f5d283dd123d1e72bf9470bd5f7
Sherin <csherin111@gmail.com>
2024-08-31 12:16:20 +0400

    Page not found component adde

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white text-center">
+      <h1 className="text-6xl font-bold mb-8">404</h1>
+      <p className="text-2xl mb-4">Page Not Found</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
